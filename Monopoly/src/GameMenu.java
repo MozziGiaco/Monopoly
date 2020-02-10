1891147d465552788940774bdc5e05b75800da4a
michael.Nguyen <michael.Nguyen@MU-C1-18.mullen.pvt.k12.co.us>
2020-02-10 11:53:59 -0700

    dices(moday feb 10, 2020)

--- Monopoly/src/GameMenu.java
+++ Monopoly/src/GameMenu.java
@@ -5,7 +5,7 @@ public class GameMenu
 		public static void main(String[] args)
 			{
 				// Emy master comment 
-
+				//MonopolyCardsandDice.dices();
 			}
 
 	}
--- Monopoly/src/MonopolyCardsandDice.java
+++ Monopoly/src/MonopolyCardsandDice.java
@@ -1,11 +1,27 @@
+import java.util.Scanner;
 
 public class MonopolyCardsandDice
 	{
 
-		public static void main(String[] args)
-			{
-				// TODO Auto-generated method stub
+		public static void dices()
+		{
+			Scanner userInput = new Scanner(System.in);
 
+			int randomNumber1 = (int) (Math.random() *6 + 1);
+			int randomNumber2 = (int) (Math.random() *6 + 1);
+			
+			System.out.println("Click enter to roll");
+			String roll = userInput.nextLine();
+			
+			int dice1 = randomNumber1;
+			int dice2 = randomNumber2; 
+			int diceSum = dice1 + dice2;
+
+			System.out.println(" You rolled: " + dice1 + " and " + dice2 + " For a total of " + diceSum + " Please roll again.");
+		}
+		public static void cards()
+			{
+				
 			}
 
 	}
