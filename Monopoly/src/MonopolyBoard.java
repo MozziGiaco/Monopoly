33be1d8e7a0599b095f8d96d41cfc181be39577e
nicholas.giacomozzi <nicholas.giacomozzi@mu-c1-03.mullen.pvt.k12.co.us>
2020-02-11 12:19:08 -0700

    Almost done with array

--- Monopoly/src/MonopolyBoard.java
+++ Monopoly/src/MonopolyBoard.java
@@ -2,9 +2,50 @@
 public class MonopolyBoard
 	{
 
-		public static void main(String[] args)
+		public static void makeArray()
 			{
-				// TODO Auto-generated method stub
+				String[] boardIndex = new String[5];
+
+				boardIndex[0] = "GO : COLLECT $200 SALARY AS YOU PASS";
+				boardIndex[1] = "MEDITERRANEAN AVENUE (PURPLE) PRICE: $60";
+				boardIndex[2] = "COMMUNITY CHEST : FOLLOW INSTRUCTIONS ON TOP CARD";
+				boardIndex[3] = "BALTIC AVENUE (PURPLE) PRICE: $60";
+				boardIndex[4] = "INCOME TAX : PAY 10% OR $200";
+				boardIndex[5] = "READING RAILROAD : PRICE: $200";
+				boardIndex[6] = "ORIENTAL AVENUE (LIGHT BLUE) PRICE: $100";
+				boardIndex[7] = "CHANCE";
+				boardIndex[8] = "VERMONT AVENUE (LIGHT BLUE) PRICE: $100";
+				boardIndex[9] = "CONNECTICUT AVENUE (LIGHT BLUE) PRICE: $120";
+				boardIndex[10] = "IN JAIL (JUST VISITING)";
+				boardIndex[11] = "ST. CHARLES (PINK) PRICE: $140";
+				boardIndex[12] = "ELECTRIC COMPANY : PRICE: $150";
+				boardIndex[13] = "STATES AVENUE : PRICE: $140";
+				boardIndex[14] = "VIRGINIA AVENUE : PRICE: $160";
+				boardIndex[15] = "PENNSYLVANIA RAILROAD : PRICE: $200";
+				boardIndex[16] = "ST. JAMES PLACE (ORANGE) PRICE: $180";
+				boardIndex[17] = "COMMUNITY CHEST : FOLLOW INSTRUCTIONS ON TOP CARD";
+				boardIndex[18] = "TENNESSEE AVENUE : PRICE: $180";
+				boardIndex[19] = "NEW YORK AVENUE : PRICE: $200";
+				boardIndex[20] = "FREE PARKING";
+				boardIndex[21] = "KENTUCKY AVENUE : PRICE: $220";
+				boardIndex[22] = "CHANCE";
+				boardIndex[23] = "INDIANA AVENUE : PRICE: $220";
+				boardIndex[24] = "ILLINOIS AVENUE : PRICE: $240";
+				boardIndex[25] = "Gray";
+				boardIndex[26] = "Navy Blue";
+				boardIndex[27] = "Yellow";
+				boardIndex[28] = "Light Blue";
+				boardIndex[29] = "White";
+				boardIndex[30] = "Gray";
+				boardIndex[31] = "Navy Blue";
+				boardIndex[32] = "Yellow";
+				boardIndex[33] = "Light Blue";
+				boardIndex[34] = "White";
+				boardIndex[35] = "Gray";
+				boardIndex[36] = "Navy Blue";
+				boardIndex[37] = "Yellow";
+				boardIndex[38] = "Light Blue";
+				boardIndex[39] = "White";
 
 			}
 
--- Monopoly/src/MonopolyRunner.java
+++ Monopoly/src/MonopolyRunner.java
@@ -4,8 +4,7 @@ public class MonopolyRunner
 
 		public static void main(String[] args)
 			{
-				// TODO Auto-generated method stub
-
+			
 			}
 
 	}
