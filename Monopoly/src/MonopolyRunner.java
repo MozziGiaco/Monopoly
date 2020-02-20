218a66fe1328a2d81d679d035e2e9f7a434b3079
nicholas.giacomozzi <nicholas.giacomozzi@mu-c1-03.mullen.pvt.k12.co.us>
2020-02-20 11:16:26 -0700

    commit test

--- Monopoly/src/MonopolyRunner.java
+++ Monopoly/src/MonopolyRunner.java
@@ -1,20 +1,16 @@
 
-
 public class MonopolyRunner
-{
-public static void main(String[] args)
-{
+	{
+		public static void main(String[] args)
+			{
 // TODO Auto-generated method stub
-GameMenu.gameIntro();
-MonopolyCardsandDice.dice();
+				GameMenu.gameIntro();
+				MonopolyCardsandDice.dice();
 //hello there
-MonopolyBank.bankerIntro();
-System.out.println(" ");
-MonopolyBank.bankMath();
-System.out.println(" ");
+				MonopolyBank.bankerIntro();
+				System.out.println(" ");
+				MonopolyBank.bankMath();
+				System.out.println(" ");
 
-}
-}
-
-
-	
+			}
+	}
