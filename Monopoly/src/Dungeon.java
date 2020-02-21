c2108a213f1205777488a1d2c28415e2709977c5
emily.deline <emily.deline@MU-C1-04.mullen.pvt.k12.co.us>
2020-02-21 12:40:22 -0700

    Finishing this tonight dont worry guys I got you but lets meet up Monday at my house maybe and really get everything done

--- Monopoly/src/Dungeon.java
+++ Monopoly/src/Dungeon.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Dungeon extends Event
 	{
 
@@ -18,7 +20,26 @@ public class Dungeon extends Event
 				if (players.index == 10)
 					{
 						System.out.println("Haha sucker you have to go pay a visit to the DUNGEON!"
-								+ "n/ and skip your next turn");
+								+ "n/ and 1 of your turns were skip");
+						System.out.println(MonopolyBank.playName.get(0).getName() + " is now on tile 5.");
+						landOn();
+					}
+			}
+		public void inDungeon()
+			{
+				if(players.index == 30)
+					{
+						System.out.println("You are now in the dungeon, " + MonopolyBank.playName.get(0).getName());
+						System.out.println("You either have to pay $200 or lose three turns.");
+						System.out.println("What is your choice..." + MonopolyBank.playName.get(0).getName()
+								+ "n/ 1) Pay $200"
+								+ "n/ 2) Lose Three Turns");
+						
+						Scanner dungeon = new Scanner(System.in);
+						if(dungeon == 1)
+							{
+								
+							}
 					}
 			}
 
--- Monopoly/src/MonopolyBank.java
+++ Monopoly/src/MonopolyBank.java
@@ -13,7 +13,11 @@ import javax.swing.JOptionPane;
 
 public class MonopolyBank
 	{
-	static ArrayList <players> playName = new ArrayList <players>();
+		static Scanner playerChoose = new Scanner(System.in);
+		static int player1Choice;
+		//static int player2Choice;
+		static ArrayList <players> playName = new ArrayList <players>();
+		static String playerName;
 	  public static void bankerIntro()
 				{
 					//introduction to the banker
@@ -44,12 +48,142 @@ public class MonopolyBank
 			
 			public static void choosePlayer()
 				{
-					System.out.println("Player 1 who you want to be! Your choises are:"
+					System.out.println("Player 1 who do you want to be? Your choises are:"
 							+ "n/ 1) Scar"
 							+ "n/ 2) Hades"
 							+ "n/ 3) Ysma"
+							+ "n/ or"
 							+ "n/ 4) Ursula");
-					
+					player1Choice = playerChoose.nextInt(); 
+						if(player1Choice == 1)
+							{
+								playerName = playName.get(0).setName("Scar");
+								System.out.println("OOOOO Scar, I guess you have to be PREPARED!");
+								System.out.println("You will now be known as Scar.");
+								System.out.println( );
+								System.out.println("Now player 2 who do you want to be? Your choises are:"
+										+ "n/ 1) Hades"
+										+ "n/ 2) Ysma"
+										+ "or "
+										+ "n/ 3) Ursula");
+								
+								//player2Choice = playerChoose.nextInt(); 
+									//if(player2Choice == 1)
+										//{
+											//System.out.println("How do you kill a god? The one question Hades is still asking.");
+											//System.out.println("You will now be known as Hades.");
+
+										//}
+									//else if(player2Choice == 2)
+										//{
+											//System.out.println("Pull the Lever Cronk, WRONG LEVER!! The master mind of postions Ysma.");
+											//System.out.println("You will now be known as Ysma.");
+
+										//}
+								//	else if(player2Choice == 3)
+											//{
+												//System.out.println("THOSE POOR UNFORTINATE SOULS that have to watch Ursula practic her BODY LANGUAGE...");
+												//System.out.println("You will now be known as Ursula.");
+
+										//	}
+								
+							}
+						
+						if(player1Choice == 2)
+							{
+								System.out.println("How do you kill a god? The one question Hades is still asking.");
+								System.out.println("You will now be known as Hades.");
+								System.out.println( );
+								//System.out.println("Now player 2 who do you want to be? Your choises are:"
+									//	+ "n/ 1) Scar"
+									//	+ "n/ 2) Ysma"
+									//	+ "or "
+									//	+ "n/ 3) Ursula");
+								//player2Choice = playerChoose.nextInt(); 
+									//if(player2Choice == 1)
+										//{
+											//System.out.println("OOOOO Scar, I guess you have to be PREPARED!");
+											//System.out.println("You will now be known as Scar.");
+
+										//}
+									//else if(player2Choice == 2)
+										//{
+											//System.out.println("Pull the Lever Cronk, WRONG LEVER!! The master mind of postions Ysma.");
+											//System.out.println("You will now be known as Ysma.");
+
+										//}
+									//else if(player2Choice == 3)
+											//{
+											//	System.out.println("THOSE POOR UNFORTINATE SOULS that have to watch Ursula practic her BODY LANGUAGE...");
+											//	System.out.println("You will now be known as Ursula.");
+
+										//	}
+								
+							}
+						
+						if(player1Choice == 3)
+								{
+									System.out.println("Pull the Lever Cronk, WRONG LEVER!! The master mind of postions Ysma.");
+									System.out.println("You will now be known as Ysma.");
+									System.out.println( );
+									//System.out.println("Now player 2 who do you want to be? Your choises are:"
+											//+ "n/ 1) Scar"
+											//+ "n/ 2) Hades"
+											//+ "or "
+											//+ "n/ 3) Ursula");
+									//player2Choice = playerChoose.nextInt(); 
+									//	if(player2Choice == 1)
+									//		{
+								//				System.out.println("OOOOO Scar, I guess you have to be PREPARED!");
+								//				System.out.println("You will now be known as Scar.");
+
+								//			}
+									//	else if(player2Choice == 2)
+								//			{
+									//			System.out.println("How do you kill a god? The one question Hades is still asking.");
+								//				System.out.println("You will now be known as Hades.");
+
+								//			}
+								//		else if(player2Choice == 3)
+								//				{
+								//					System.out.println("THOSE POOR UNFORTINATE SOULS that have to watch Ursula practic her BODY LANGUAGE...");
+								//					System.out.println("You will now be known as Ursula.");
+
+								//				}
+									
+								}
+						
+							if(player1Choice == 4)
+									{
+										System.out.println("THOSE POOR UNFORTINATE SOULS that have to watch Ursula practic her BODY LANGUAGE...");
+										System.out.println("You will now be known as Ursula.");
+										System.out.println( );
+										//System.out.println("Now player 2 who do you want to be? Your choises are:"
+										//		+ "n/ 1) Scar"
+										//		+ "n/ 2) Hades"
+									//			+ "or "
+										//		+ "n/ 3) Ysma");
+									//	player2Choice = playerChoose.nextInt(); 
+									//		if(player2Choice == 1)
+									//			{
+									//				System.out.println("OOOOO Scar, I guess you have to be PREPARED!");
+									//				System.out.println("You will now be known as Scar.");
+
+									//			}
+									//		else if(player2Choice == 2)
+									//			{
+									//				System.out.println("How do you kill a god? The one question Hades is still asking.");
+									//				System.out.println("You will now be known as Hades.");
+
+									//			}
+									//		else if(player2Choice == 3)
+									//				{
+									//					System.out.println("Pull the Lever Cronk, WRONG LEVER!! The master mind of postions Ysma.");
+									//					System.out.println("You will now be known as Ysma.");
+
+									//				}
+										
+									}
 					
 					playName.add(new players(1500.00, "Scar", 0));
 					playName.add(new players(1500.00, "Hades", 0));
