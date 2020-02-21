7df2afc004ad22dd19ed65b18b379bfaca833cac
nicholas.giacomozzi <nicholas.giacomozzi@mu-c1-03.mullen.pvt.k12.co.us>
2020-02-21 11:36:51 -0700

    FIXED

--- Monopoly/src/Dungeon.java
+++ Monopoly/src/Dungeon.java
@@ -1,40 +1,25 @@
 public class Dungeon extends Event
 	{
 
-
-public Dungeon(String name)
-	{
-		super(name);
-		
-	}
-
-@Override
-	public void triggerEvent()
-		{
-			visitingDungeon();
-		}
-
-public void visitingDungeon()
-	{
-		if(players.index == 10)
-			{
-				System.out.println("Haha sucker you have to go pay a visit to the DUNGEON!"
-						+ "n/ and skip your next turn");
-			}
-	}
-
 		public Dungeon(String name)
 			{
 				super(name);
-// TODO Auto-generated constructor stub
+
 			}
 
 		@Override
 		public void triggerEvent()
 			{
-// TODO Auto-generated method stub
-
+				visitingDungeon();
 			}
 
+		public void visitingDungeon()
+			{
+				if (players.index == 10)
+					{
+						System.out.println("Haha sucker you have to go pay a visit to the DUNGEON!"
+								+ "n/ and skip your next turn");
+					}
+			}
 
 	}
