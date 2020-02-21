8913ea6b031a0577716386a041b7996074ae1b25
emma.mcguire <emma.mcguire@MU-C1-05.mullen.pvt.k12.co.us>
2020-02-21 11:35:52 -0700

    more pix

--- Monopoly/src/MonopolyPlay.java
+++ Monopoly/src/MonopolyPlay.java
@@ -20,90 +20,102 @@ public class MonopolyPlay
 			if (players.index + MonopolyCardsandDice.diceSum == 1)
 		    	{
 		    		 ImageIcon cottage = new ImageIcon(("cottage.jpg"));
-					    JOptionPane.showMessageDialog(frame, "You have landed on AURORA'S COTTAGE (PURPLE) PRICE: $60", null, JOptionPane.QUESTION_MESSAGE, cottage);
+					    JOptionPane.showMessageDialog(frame, "You have landed on AURORA'S COTTAGE", null, JOptionPane.QUESTION_MESSAGE, cottage);
 		    	}
 			
 			if (players.index + MonopolyCardsandDice.diceSum == 3)
 		    	{
 		    		 ImageIcon sleepingBeautysCastle = new ImageIcon(("sleepingBeautysCastle.jpg"));
-					    JOptionPane.showMessageDialog(frame, "You have landed on SLEEPING BEAUTY CASTLE (PURPLE) PRICE: $60", null, JOptionPane.QUESTION_MESSAGE, sleepingBeautysCastle);
+					    JOptionPane.showMessageDialog(frame, "You have landed on SLEEPING BEAUTY CASTLE", null, JOptionPane.QUESTION_MESSAGE, sleepingBeautysCastle);
 		    	}
 			
 			if (players.index + MonopolyCardsandDice.diceSum == 6)
 		    	{
 		    		 ImageIcon badlands = new ImageIcon(("badlands.jpg"));
-					    JOptionPane.showMessageDialog(frame, "You have landed on THE BADLANDS (LIGHT BLUE) PRICE: $100", null, JOptionPane.QUESTION_MESSAGE, badlands);
+					    JOptionPane.showMessageDialog(frame, "You have landed on THE BADLANDS", null, JOptionPane.QUESTION_MESSAGE, badlands);
 		    	}
 			if (players.index + MonopolyCardsandDice.diceSum == 8)
 		    	{
 		    		 ImageIcon falls = new ImageIcon(("falls.jpg"));
-					    JOptionPane.showMessageDialog(frame, "You have landed on HAKUNA MATATA FALLS (LIGHT BLUE) PRICE: $100", null, JOptionPane.QUESTION_MESSAGE, falls);
+					    JOptionPane.showMessageDialog(frame, "You have landed on HAKUNA MATATA FALLS", null, JOptionPane.QUESTION_MESSAGE, falls);
 		    	}
 			if (players.index + MonopolyCardsandDice.diceSum == 9)
 		    	{
 		    		 ImageIcon prideRock = new ImageIcon(("prideRock.jpg"));
-					    JOptionPane.showMessageDialog(frame, "You have landed on PRIDE ROCK (LIGHT BLUE) PRICE: $120", null, JOptionPane.QUESTION_MESSAGE, prideRock);
+					    JOptionPane.showMessageDialog(frame, "You have landed on PRIDE ROCK", null, JOptionPane.QUESTION_MESSAGE, prideRock);
 		    	}
 			if (players.index + MonopolyCardsandDice.diceSum == 11)
 		    	{
 		    		 ImageIcon snugglyDuckling = new ImageIcon(("snugglyDuckling.jpg"));
-					    JOptionPane.showMessageDialog(frame, "You have landed on THE SNUGGLY DUCKLING  (PINK) PRICE: $140", null, JOptionPane.QUESTION_MESSAGE, snugglyDuckling);
+					    JOptionPane.showMessageDialog(frame, "You have landed on THE SNUGGLY DUCKLING", null, JOptionPane.QUESTION_MESSAGE, snugglyDuckling);
 		    	}
 			if (players.index + MonopolyCardsandDice.diceSum == 13)
 		    	{
 		    		 ImageIcon tower = new ImageIcon(("tower.jpg"));
-					    JOptionPane.showMessageDialog(frame, "You have landed on RAPUNZEL'S TOWER (PINK) : PRICE: $140", null, JOptionPane.QUESTION_MESSAGE, tower);
+					    JOptionPane.showMessageDialog(frame, "You have landed on RAPUNZEL'S TOWER", null, JOptionPane.QUESTION_MESSAGE, tower);
 		    	}
 			if (players.index + MonopolyCardsandDice.diceSum == 14)
 		    	{
 		    		 ImageIcon corona = new ImageIcon(("corona.jpg"));
-					    JOptionPane.showMessageDialog(frame, "You have landed on CORONA CASTLE (PINK) : PRICE: $160", null, JOptionPane.QUESTION_MESSAGE, corona);
+					    JOptionPane.showMessageDialog(frame, "You have landed on CORONA CASTLE", null, JOptionPane.QUESTION_MESSAGE, corona);
 		    	}
 			if (players.index + MonopolyCardsandDice.diceSum == 16)
 		    	{
 		    		 ImageIcon bog = new ImageIcon(("bog.jpg"));
-					    JOptionPane.showMessageDialog(frame, "You have landed on THE BOG (ORANGE) PRICE: $180", null, JOptionPane.QUESTION_MESSAGE, bog);
+					    JOptionPane.showMessageDialog(frame, "You have landed on THE BOG", null, JOptionPane.QUESTION_MESSAGE, bog);
 		    	}
 			if (players.index + MonopolyCardsandDice.diceSum == 18)
 		    	{
 		    		 ImageIcon ferry = new ImageIcon(("ferry.jpg"));
-					    JOptionPane.showMessageDialog(frame, "You have landed on THE FERRY : PRICE: $180", null, JOptionPane.QUESTION_MESSAGE, ferry);
+					    JOptionPane.showMessageDialog(frame, "You have landed on THE FERRY", null, JOptionPane.QUESTION_MESSAGE, ferry);
 		    	}
 			if (players.index + MonopolyCardsandDice.diceSum == 19)
 		    	{
 		    		 ImageIcon tiana = new ImageIcon(("tiana.jpg"));
-					    JOptionPane.showMessageDialog(frame, "You have landed on TIANA'S PLACE : PRICE: $200", null, JOptionPane.QUESTION_MESSAGE, tiana);
+					    JOptionPane.showMessageDialog(frame, "You have landed on TIANA'S PLACE", null, JOptionPane.QUESTION_MESSAGE, tiana);
 		    	}
 			if (players.index + MonopolyCardsandDice.diceSum == 21)
 		    	{
 		    		 ImageIcon jungle = new ImageIcon(("jungle.jpg"));
-					    JOptionPane.showMessageDialog(frame, "You have landed on THE JUNGLE (RED) : PRICE: $220", null, JOptionPane.QUESTION_MESSAGE, jungle);
+					    JOptionPane.showMessageDialog(frame, "You have landed on THE JUNGLE", null, JOptionPane.QUESTION_MESSAGE, jungle);
 		    	}
 			if (players.index + MonopolyCardsandDice.diceSum == 23)
 		    	{
 		    		 ImageIcon pacha = new ImageIcon(("pacha.jpg"));
-					    JOptionPane.showMessageDialog(frame, "You have landed on PACHA'S VILLAGE (RED) : PRICE: $220", null, JOptionPane.QUESTION_MESSAGE, pacha);
+					    JOptionPane.showMessageDialog(frame, "You have landed on PACHA'S VILLAGE", null, JOptionPane.QUESTION_MESSAGE, pacha);
 		    	}
 			if (players.index + MonopolyCardsandDice.diceSum == 24)
 		    	{
 		    		 ImageIcon kuzco = new ImageIcon(("kuzco.jpg"));
-					    JOptionPane.showMessageDialog(frame, "You have landed on KUZCO'S PALACE (RED) : PRICE: $240", null, JOptionPane.QUESTION_MESSAGE, kuzco);
+					    JOptionPane.showMessageDialog(frame, "You have landed on KUZCO'S PALACE", null, JOptionPane.QUESTION_MESSAGE, kuzco);
 		    	}
 			if (players.index + MonopolyCardsandDice.diceSum == 26)
 		    	{
 		    		 ImageIcon belleVillage = new ImageIcon(("belleVillage.jpg"));
-					    JOptionPane.showMessageDialog(frame, "You have landed on BELLE'S VILLAGE (YELLOW) : PRICE: $260", null, JOptionPane.QUESTION_MESSAGE, belleVillage);
+					    JOptionPane.showMessageDialog(frame, "You have landed on BELLE'S VILLAGE", null, JOptionPane.QUESTION_MESSAGE, belleVillage);
 		    	}
 			if (players.index + MonopolyCardsandDice.diceSum == 27)
 		    	{
 		    		 ImageIcon woods = new ImageIcon(("woods.jpg"));
-					    JOptionPane.showMessageDialog(frame, "You have landed on WOLVES' WOODS : PRICE: $260", null, JOptionPane.QUESTION_MESSAGE, woods);
+					    JOptionPane.showMessageDialog(frame, "You have landed on WOLVES' WOODS", null, JOptionPane.QUESTION_MESSAGE, woods);
 		    	}
 			
 			if (players.index + MonopolyCardsandDice.diceSum == 29)
 		    	{
 		    		 ImageIcon beast = new ImageIcon(("beast.jpg"));
-					    JOptionPane.showMessageDialog(frame, "You have landed on THE BEAST'S CASTLE (YELLOW) : PRICE: $280", null, JOptionPane.QUESTION_MESSAGE, beast);
+					    JOptionPane.showMessageDialog(frame, "You have landed on THE BEAST'S CASTLE", null, JOptionPane.QUESTION_MESSAGE, beast);
+		    	}
+			
+			if (players.index + MonopolyCardsandDice.diceSum == 31)
+		    	{
+		    		 ImageIcon lair = new ImageIcon(("lair.jpg"));
+					    JOptionPane.showMessageDialog(frame, "You have landed on URSULA'S LAIR", null, JOptionPane.QUESTION_MESSAGE, lair);
+		    	}
+			if (players.index + MonopolyCardsandDice.diceSum == 32)
+		    	{
+		    		 ImageIcon ship = new ImageIcon(("ship.jpg"));
+					    JOptionPane.showMessageDialog(frame, "You have landed on ERIC'S SHIP", null, JOptionPane.QUESTION_MESSAGE, ship);
+		    	
 		    	}
 			
 			
