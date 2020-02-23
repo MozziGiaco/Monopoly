653fb52c134e16830bad638d9871d678cb0e0910
emdeline <emdeline@10.0.0.156>
2020-02-23 12:43:55 -0700

    Everything is being called now when it is landing on a certain tile, this is being coded for in the MonopolyPlay class if you want to go check that out

--- Monopoly/src/Utility.java
+++ Monopoly/src/Utility.java
@@ -11,17 +11,17 @@ public class Utility extends Purchaseable
 // TODO Auto-generated constructor stub
 			}
 
-		public void landOnUtility()
+		public static void landOnUtility()
 			{
 				choice3 = intInput.nextInt();
-				System.out.println("Would you like to buy " + name + "?");
+				System.out.println("Would you like to buy " + players.index + "?");
 
 				System.out.println("(1) Yes");
 				System.out.println("(2) No");
 
 				if (choice3 == 1)
 					{
-						System.out.println("You have chosen to purchase " + name + ".");
+						System.out.println("You have chosen to purchase " + players.index + ".");
 						MonopolyBank.playName.get(0).setMoney(
 								MonopolyBank.playName.get(0).getMoney() - MonopolyRunner.board[players.index].price);
 
